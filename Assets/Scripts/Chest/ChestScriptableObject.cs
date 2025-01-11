a0fa9ebf4a5f8da81ed5958440d9e800b282d572
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2025-01-11 11:17:10 +0530

    created chest data scriptable object

---
+++ Assets/Scripts/Chest/ChestScriptableObject.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ChestScriptableObject", menuName = "ScriptableObjects/ChestScriptableObject")]
+public class ChestScriptableObject : ScriptableObject
+{
+    [Header("Chest Properties")]
+    public ChestType ChestType;
+
+    [Header("Rewards")]
+    public RangeInt CoinRange;
+    public RangeInt GemRange;
+
+    [Header("Unlocking Time")]
+    public int UnlockTime;
+}
+
+[System.Serializable]
+public struct RangeInt
+{
+    public int Min;
+    public int Max;
+
+    public RangeInt(int min, int max)
+    {
+        Min = min;
+        Max = max;
+    }
+
+    /// <summary>
+    /// Get a random value within the range.
+    /// </summary>
+    public int GetRandomValue() => Random.Range(Min, Max + 1);
+}
