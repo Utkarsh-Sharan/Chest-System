72cccaec4e54610d0874655daef46a978c53ceb2
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2025-01-12 10:50:50 +0530

    designed chest button

--- Assets/Scripts/Slot/SlotController.cs
+++ Assets/Scripts/Slot/SlotController.cs
@@ -3,14 +3,11 @@ using UnityEngine.UI;
 
 public class SlotController : MonoBehaviour
 {
-    private SlotState slotState;
     [SerializeField] private int maximumSlots;
     [SerializeField] private GameObject emptySlotObject;
 
     private void Start()
     {
-        slotState = SlotState.Empty;
-
         for (int i = 0; i < maximumSlots; ++i)
         {
             Instantiate(emptySlotObject, this.transform);
