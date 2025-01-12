252199263b9da742390be63b3cbb9fb730916c77
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2025-01-12 12:06:32 +0530

    trying to create random chest type

--- Assets/Scripts/Chest/ChestController.cs
+++ Assets/Scripts/Chest/ChestController.cs
@@ -1,18 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ChestController : MonoBehaviour
+public class ChestController
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private ChestView chestView;
+    private ChestScriptableObject chestSO;
 
-    // Update is called once per frame
-    void Update()
+    public void CreateRandomChest()
     {
-        
+
     }
 }
---
+++ Assets/Scripts/UI/UIService.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIService : MonoBehaviour
+{
+    [SerializeField] private Button generateChestButton;
+
+    private void Start()
+    {
+        generateChestButton.onClick.AddListener(GenerateRandomChest);
+    }
+
+    private void GenerateRandomChest()
+    {
+
+    }
+}
